8a0d68a98bb4a1f5fe82413a645e768179f892bc
Nick <alexhh1204@hotmail.com>
2024-10-17 21:33:06 -0600

    'Hola'

--- app/router.js
+++ app/router.js
@@ -6,4 +6,10 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('citas-portal');
+
+  this.route('administrador', function () {
+    this.route('usuarios');
+  });
+});
